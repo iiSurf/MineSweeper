5c7cfa31d8b1d9cd998ae43679a6b1a44bb4fddb
iiSurf <nicksandberg@hotmail.com>
2026-06-27 19:41:27 -0700

    added if/else logic for board and what to print

--- Minesweeper.java
+++ Minesweeper.java
@@ -32,7 +32,11 @@ public class Minesweeper {
         };
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j]);
+                if (board[i][j] == '*') {
+                    System.out.print(board[i][j]);
+                } else {
+                    System.out.println('.');
+                }
             }
             System.out.println();
         }
